02708e746ee8f48f617bb0c2960c3cab56374f70
bizui <bizuilize@gmail.com>
2016-06-07 22:25:13 -0400

    add ACAM map

--- Gruntfile.js
+++ Gruntfile.js
@@ -189,8 +189,8 @@ module.exports = function(grunt) {
                     './src/map_data/total-high-cost-distributions-rate-return-study-areas.json',
                     './src/map_data/total-line-usf-high-cost-distributions-rate-return-study-areas.json',
                     './src/map_data/net-usf-received-voice-line.json',
-                    './src/map_data/rural-health-care-pilot-program.json'
-                    //'./src/map_data/acam-ror-sa-map.json'
+                    './src/map_data/rural-health-care-pilot-program.json',
+                    './src/map_data/acam-ror-sa-map.json'
                 ],
                 dest: './src/content.json'
             }           
